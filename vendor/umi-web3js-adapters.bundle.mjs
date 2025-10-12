9deae3823428f800c888db2270ca05c492b6f890
smitskecbs <smitskecbs@gmail.com>
2025-10-12 14:01:43 +0200

    Create umi-web3js-adapters.bundle.mjs

---
+++ vendor/umi-web3js-adapters.bundle.mjs
@@ -0,0 +1,5 @@
+/* esm.sh - @metaplex-foundation/umi-web3js-adapters@1.4.1 */
+import "/@metaplex-foundation/umi@^1.4.1?target=es2022";
+import "/@solana/web3.js@^1.72.0?target=es2022";
+import "/node/buffer.mjs";
+export * from "/@metaplex-foundation/umi-web3js-adapters@1.4.1/es2022/umi-web3js-adapters.bundle.mjs";
