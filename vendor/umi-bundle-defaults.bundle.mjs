8f54823fda760b20b1f846fb7fbea13dc622dba6
smitskecbs <smitskecbs@gmail.com>
2025-10-12 13:58:16 +0200

    Create umi-bundle-defaults.bundle.mjs

---
+++ vendor/umi-bundle-defaults.bundle.mjs
@@ -0,0 +1,6 @@
+/* esm.sh - @metaplex-foundation/umi-bundle-defaults@1.4.1 */
+import "/@metaplex-foundation/umi@^1.4.1/serializers?target=es2022";
+import "/@metaplex-foundation/umi@^1.4.1?target=es2022";
+import "/@solana/web3.js@^1.72.0?target=es2022";
+import "/node/buffer.mjs";
+export * from "/@metaplex-foundation/umi-bundle-defaults@1.4.1/es2022/umi-bundle-defaults.bundle.mjs";
