1e1654b9932d2ca7d2905448ebee38578ac40970
smitskecbs <smitskecbs@gmail.com>
2025-11-11 07:03:43 +0100

    Update holders.js

--- api/holders.js
+++ api/holders.js
@@ -1,3 +1,21 @@
-export default function handler(req, res) {
-  res.status(200).json({ ok: true, now: Date.now() });
+export default async function handler(req, res) {
+  const apiKey = process.env.BIRDEYE_KEY; // haalt key veilig uit Vercel
+
+  try {
+    const response = await fetch(
+      "https://public-api.birdeye.so/public/tokenlist?chain=solana",
+      {
+        headers: {
+          accept: "application/json",
+          "x-chain": "solana",
+          "X-API-KEY": apiKey,efbead31f6db4087a18841c3bf32d0c6
+        },
+      }
+    );
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
